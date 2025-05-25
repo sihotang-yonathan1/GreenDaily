f989d94ff9d4ceba1525f32591bd38071d8c1b95
Yonathan Sihotang <143815443+sihotang-yonathan1@users.noreply.github.com>
2025-05-25 19:13:33 +0800

    feat: Use `react-router` for routing path

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { BrowserRouter, Route, Routes } from "react-router";
+
 import './global.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />}/>
+      </Routes>
+    </BrowserRouter>    
   </React.StrictMode>,
 )
 
