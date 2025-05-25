e2051082888fd34de74c28d75f6b25b96ab74885
Yonathan Sihotang <143815443+sihotang-yonathan1@users.noreply.github.com>
2025-05-25 19:25:44 +0800

    feat: Set alyout route to dashboard

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,16 @@ import App from './App.tsx'
 import { BrowserRouter, Route, Routes } from "react-router";
 
 import './global.css'
+import { DashboardLayout } from './app/index.tsx';
+import { DailyChallengePage } from './app/dailyChallenge/index.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<App />}/>
+        <Route path='/' element={<DashboardLayout />}>
+          <Route index element={<DailyChallengePage />}/>
+        </Route>
       </Routes>
     </BrowserRouter>    
   </React.StrictMode>,
