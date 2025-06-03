e35642e2a51a02c6ecd96358d9c6fdb8d4f83745
sihotang-yonathan1 <143815443+sihotang-yonathan1@users.noreply.github.com>
2025-06-03 10:59:32 +0800

    feat: Add Calculator sub-app basic functionality (#4)

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,20 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from "react-router";
 
 import './global.css'
+
+// Dashboard
 import { DashboardLayout } from './app/index.tsx';
+
+// DailyChallenge
 import { DailyChallengePage } from './app/dailyChallenge/index.tsx';
+
+// Reminder
 import { ReminderPage } from './app/reminder/index.tsx';
+
+// Calculator
+import { CalculatorLayout } from './app/energyCalculator/layout.tsx';
+import { CalculatorByPrice } from './app/energyCalculator/byPrice.tsx';
+import { CalculatorByDevice } from './app/energyCalculator/byDevice.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -14,7 +25,12 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <Route element={<DashboardLayout />} >
           <Route index element={<DailyChallengePage />}/>
           <Route path='reminder' element={<ReminderPage />} />
+          <Route path='calculator' element={<CalculatorLayout/>}>
+            <Route index element={<CalculatorByPrice />} />
+            <Route path="byDevice" element={<CalculatorByDevice />} />
+          </Route>
         </Route>
+        
       </Routes>
     </BrowserRouter>    
   </React.StrictMode>,
