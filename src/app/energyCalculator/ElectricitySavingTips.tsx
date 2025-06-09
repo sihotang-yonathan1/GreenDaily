2d6ad561064cb94933cf98a489a05f8424d47e4d
Yonathan Sihotang <143815443+sihotang-yonathan1@users.noreply.github.com>
2025-06-09 20:08:04 +0800

    feat: Use OOP approach to declare item in tips item

--- src/app/energyCalculator/ElectricitySavingTips.tsx
+++ src/app/energyCalculator/ElectricitySavingTips.tsx
@@ -7,16 +7,28 @@ interface ElectricitySavingTipsProps {
   // currentCalculationResult?: number;
 }
 
-type TipsBoxRowProps = {
+interface TipsBoxRowProps {
   message: string;
 }
 
-export function TipsBoxRow({message}: TipsBoxRowProps){
+class TipsClass implements TipsBoxRowProps {
+  message: string;
+
+  constructor (message: string){
+    this.message = message;
+  }
+
+  showMessage(){
+    console.log(this.message)
+  }
+}
+
+export function TipsBoxRow({item}: {item: TipsClass}){
   return (
     <div className='flex flex-row px-1 border w-full h-12 py-1'>
       {/* TODO: icon */}
       <div className='flex flex-row items-center'>
-        <p className='text-gray-700 text-sm'>{message}</p>
+        <p className='text-gray-700 text-sm'>{item.message}</p>
       </div>
     </div>
   )
@@ -25,19 +37,19 @@ export function TipsBoxRow({message}: TipsBoxRowProps){
 export const ElectricitySavingTips: React.FC<ElectricitySavingTipsProps> = () => {
   // Contoh tips statis
   const tips = [
-    "Matikan lampu saat tidak digunakan.",
-    "Cabut charger perangkat jika tidak digunakan.",
-    "Gunakan AC pada suhu 24-26 derajat Celsius.",
-    "Bersihkan kulkas secara teratur untuk efisiensi.",
-    "Manfaatkan cahaya alami di siang hari.",
-    "Gunakan peralatan elektronik hemat energi (label Energy Star)."
+    new TipsClass("Matikan lampu saat tidak digunakan."),
+    new TipsClass("Cabut charger perangkat jika tidak digunakan."),
+    new TipsClass("Gunakan AC pada suhu 24-26 derajat Celsius."),
+    new TipsClass("Bersihkan kulkas secara teratur untuk efisiensi."),
+    new TipsClass("Manfaatkan cahaya alami di siang hari."),
+    new TipsClass("Gunakan peralatan elektronik hemat energi (label Energy Star)."),
   ];
 
   return (
     <div className="p-4 bg-gray-50 rounded-lg shadow-sm">
       <h4 className="text-lg font-semibold mb-3 text-gray-800">Tips Pengurangan Biaya Listrik</h4>
       <div className='flex flex-col gap-y-1'>
-        {tips.map((item, index) => <TipsBoxRow key={index} message={item}/>)}
+        {tips.map((item, index) => <TipsBoxRow key={index} item={item}/>)}
       </div>
       {/* Anda bisa menambahkan logika untuk tips dinamis di sini */}
       {/* {props.currentCalculationResult && props.currentCalculationResult > 100000 && (
