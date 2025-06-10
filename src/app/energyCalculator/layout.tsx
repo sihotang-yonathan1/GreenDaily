5746ff3e9f1ac68f619c7083e9ade16ea8ba2682
sihotang-yonathan1 <143815443+sihotang-yonathan1@users.noreply.github.com>
2025-06-10 13:20:50 +0800

    fix: fix calculator tab active color (#30)

--- src/app/energyCalculator/layout.tsx
+++ src/app/energyCalculator/layout.tsx
@@ -4,9 +4,26 @@ export function CalculatorLayout(){
   return (
     <div className="flex flex-col h-full">
       {/* Tab */}
-      <div className="flex flex-row w-full bg-amber-400 py-2 gap-x-2 justify-between px-[20%]">
-        <NavLink to="/calculator">Price</NavLink>
-        <NavLink to="byDevice">Device</NavLink>
+      <div className="flex flex-row w-full py-2 gap-x-2 justify-between px-[20%]">
+        <NavLink 
+          to="/calculator"
+          end
+          className={({ isActive }) =>
+            `flex flex-col border p-2 w-full sm:w-24 h-8 text-wrap justify-center overflow-hidden rounded-2xl text-center items-center ${
+              isActive ? 'bg-[#a7e8ab] text-gray-900 font-semibold' : 'text-black'
+            }`
+          }
+        >Price</NavLink>
+
+        <NavLink 
+          className={({ isActive }) =>
+            `flex flex-col border p-2 w-full sm:w-24 h-8 text-wrap justify-center overflow-hidden rounded-2xl text-center items-center ${
+              isActive ? 'bg-[#a7e8ab] text-gray-900 font-semibold' : 'text-black'
+            }`
+          }
+          to="/calculator/byDevice"
+          end
+          >Device</NavLink>
       </div>
       {/* Child */}
       <Outlet />
