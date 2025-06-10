f0e9d88a1c12ea0858e46f0b1e6ea4fa7837cf69
Yonathan Sihotang <143815443+sihotang-yonathan1@users.noreply.github.com>
2025-06-10 14:56:07 +0800

    feat: Set `Card` for main `GreenDaily`

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,6 +2,7 @@ import { PropsWithChildren } from "react";
 import { ReminderLayout } from "./reminder/layout";
 import { NavLink, Outlet } from "react-router";
 import { DailyTip } from "./reminder/tips";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 export function MainAppGenericLayout({children}: PropsWithChildren){
   return (
@@ -16,13 +17,15 @@ export function DashboardLayout() {
     <div className="flex w-full h-screen bg-[#90C67C] py-2 px-4">
       {/* Main Content Section (60% of the screen width) */}
       <div className="flex flex-col w-full md:w-[60vw]">
-        <div className="flex flex-col h-[85vh] bg-blue-50 p-1 overflow-y-auto">
-          <div className="flex py-2">
-            <h3 className="text-2xl font-semibold">GreenDaily</h3>
-          </div>
+        <Card className="flex flex-col h-[85vh] bg-blue-50 p-1 overflow-y-auto">
+          <CardHeader className="flex py-2">
+            <CardTitle className="text-2xl font-semibold">GreenDaily</CardTitle>
+          </CardHeader>
 
-          <Outlet />
-        </div>
+          <CardContent>
+            <Outlet />
+          </CardContent>
+        </Card>
 
         {/* NavBar (with responsive layout) */}
         <div className="flex flex-col gap-y-1 my-2">
